f54c059ef0b014ed4b1457b9b77622f8e2cac63d
Oaktree <oaktree333@gmail.com>
2013-06-06 15:05:44 -0400

    Testing resize function

--- js/main.js
+++ js/main.js
@@ -9,17 +9,20 @@ $(document).ready(function() {
 	};
 
 	$("#View").click(function() {
-		/*
-		 $("#contentBox").load('./includehtml/resume.html', function(response, status, xhr){
-		 if(status == "error"){
-		 alert("Error: " + xhr.status + " " + xhr.statusText);
-		 }
-		 });
-		 */
 		$("#contentBox").empty();
 		$("#contentBox").append('<embed src="./pdf/resume.pdf" style = "position:absoltue; width:100%; height:100%">');
 	});
 	$("#PP").click({page: "includehtml/pump.html"}, addContent);
 	$("#Robartics").click({page: "includehtml/robartics.html"}, addContent);
 	$("#HW").click({page: "includehtml/hotwheels.html"}, addContent);
+	
+	$("#contentBox").resize(function(){
+		var contentBox =$("contentBox");
+		if($("body").width()*0.2 > 200){
+			contentBox.css("left", "200px");
+		}
+		else{
+			contentBox.css("left", "20%");
+		}
+	});
 });
