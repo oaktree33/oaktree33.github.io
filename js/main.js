3259437b1b85b16e74c494df10d50f37faaa7f56
Oaktree <oaktree333@gmail.com>
2013-06-12 18:11:32 -0400

    adding contact button link and home button link

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 		});
 	};
 
+	//adding contents when respective menu items are pressed.
 	$("#View").click(function() {
 		$("#contentBox").empty();
 		$("#contentBox").append('<embed src="./pdf/resume.pdf" style = "position:absoltue; width:100%; height:100%">');
@@ -15,7 +16,10 @@ $(document).ready(function() {
 	$("#PP").click({page: "includehtml/pump.html"}, addContent);
 	$("#Robartics").click({page: "includehtml/robartics.html"}, addContent);
 	$("#HW").click({page: "includehtml/hotwheels.html"}, addContent);
+	$("#Home").click({page: "includehtml/home.html"}, addContent);
+	$("#contact").click({page: "includehtml/contact.html"}, addContent);
 	
+	//Setting Minium and maximum position of the contentbox
 	$(window).resize(function(){
 		if($(window).width()*0.2 > 200){
 			$("#contentBox").css("left", "200px");
@@ -28,6 +32,8 @@ $(document).ready(function() {
 		}
 		
 	});
+	
+	//initially adding home page.
 	$("#contentBox").load("includehtml/home.html", function(response, status, xhr) {
 			if (status == "error") {
 				alert("Error: " + xhr.status + " " + xhr.statusText);
